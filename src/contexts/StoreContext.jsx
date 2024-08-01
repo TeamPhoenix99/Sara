a0cc607d4eb48ec74b615a2c62aa188fd721a165
TeamPhoenix99 <teamphoenix1206@gmail.com>
2024-08-01 23:06:25 +0545

    context

--- src/contexts/StoreContext.jsx
+++ src/contexts/StoreContext.jsx
@@ -7,15 +7,28 @@ export const StoreContext = createContext(null);
 export const StoreContextPrvider = (props) => {
 
     const [apiData,setApiData] = useState('')
+    const [places,setPlaces] = useState([])
+    const [activities,setActivities] = useState([])
 
     useEffect(()=>{
         
         const fetchData = async ()=> {
             let data = await getData();
-            setApiData(data);
+            setApiData(data); 
+            data.map(()=>{
+                console.log(data);
+            })
         }
         fetchData()
+        // apiData.forEach(e =>{
+        //     if(e.type === 'place'){
+        //         setPlaces([...places, e]);
+        //     }
+        // })
+        
     },[]);
+    
+
 
 
     const conextValue = {apiData}
