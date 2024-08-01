cc38e0a911d753d460e4e537bb896a6f4ca85443
TeamPhoenix99 <teamphoenix1206@gmail.com>
2024-08-02 00:05:18 +0545

    datafetch

--- src/Pages/Activities/Activities.jsx
+++ src/Pages/Activities/Activities.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { TopCategory } from '../../Components/TopCategory/TopCategory'
 import { Category } from '../../Components/Category/Category'
 import { CiSearch } from "react-icons/ci";
-import activitieshero from '../../assets/images/activityhero.jfif'
+import activitieshero from '../../assets/images/activityhero.jfif';
+import { StoreContext } from '../../contexts/StoreContext';
+
 
 export const Activities = () => {
+
+  const {apiData} = useContext(StoreContext); 
+
   return (
     <div>
       <TopCategory img={activitieshero} Utext="Try Something New" Ltext="With HolidayNepal"/>
@@ -13,7 +18,15 @@ export const Activities = () => {
         <input type="text" placeholder='Enter activities'  />
         <CiSearch />
         </div>
-      <Category />
+        <div className='place-grid'>
+        {
+          apiData.map((place,index) => (
+            <div key={index}>
+             { place.type === 'activities' ? <Category name={place.name} rating={place.rating} img={place.image} /> : <></>}
+            </div>
+          ))
+        }
+        </div>
     </div>
   )
 }
--- src/Pages/Event/Event.jsx
+++ src/Pages/Event/Event.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { TopCategory } from '../../Components/TopCategory/TopCategory'
 import { Category } from '../../Components/Category/Category'
 import { CiSearch } from "react-icons/ci";
 import eventhero from '../../assets/images/eventhero.jpg'
+import { StoreContext } from '../../contexts/StoreContext';
+
 
 export const Event = () => {
+
+  const {apiData} = useContext(StoreContext); 
+
   return (
     <div>
        <TopCategory img={eventhero} Utext="Enjoy Our Cultures and Traditions" Ltext="With HolidayNepal" />
@@ -13,7 +18,15 @@ export const Event = () => {
         <input type="text" placeholder='Enter Events'  />
         <CiSearch />
         </div>
-       <Category />
+        <div className='place-grid'>
+        {
+          apiData.map((place,index) => (
+            <div key={index}>
+             { place.type === 'events' ? <Category name={place.name} rating={place.rating} img={place.image} /> : <></>}
+            </div>
+          ))
+        }
+        </div>
     </div>
   )
 }
