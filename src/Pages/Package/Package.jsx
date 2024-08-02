1c622ae7b7c1969ac5eb83fdacf663a0d1289eaa
TeamPhoenix99 <teamphoenix1206@gmail.com>
2024-08-02 16:10:01 +0545

    AAA

--- src/Pages/Package/Package.jsx
+++ src/Pages/Package/Package.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import { getPlan } from '../../assets/data'
 import './Package.css'
+import { TripInfo } from '../../Components/OutputComponent/TripInf/TripInfo'
+import { Task } from '../../Components/OutputComponent/Task/Task'
 import { PackageLayout } from './PackageLayout'
+import { Map } from '../../Components/OutputComponent/Map/Map'
+import { FaStar } from "react-icons/fa";
 import { TripLayout } from './TripLayout'
 
-  
 
 export const Package = () => {
  
   const [data, setData] = useState([])
- 
+  const [showdetail, setShowDetail] = useState('info');
+
 
 
   useEffect(()=>{
@@ -24,13 +28,61 @@ export const Package = () => {
 
   
   
-  return (
-    <div>
-       <PackageLayout/>
-       <TripLayout/>
-    </div>
-  )
-}
+    return (
+      <div>
+        <div className='info-btn'>
+          <button value={showdetail} onClick={()=>setShowDetail('info')} className={showdetail === 'info' ? 'active-info':''}>Trip Info</button>
+          <button value={showdetail} onClick={()=>setShowDetail('task')} className={showdetail === 'task' ? 'active-info':''}>Bookings</button>
+        </div>
+        {
+          showdetail === 'info' ? <>
+            
+              {
+                data.map((dailyTask, i)=>(
+                  <>
+                <div className="day-desc">
+                <h1 className='dayNo'>Day {i +1}</h1>
+                {
+                dailyTask.map((task, index)=>(
+                  <>
+                  <div className="plan">
+                  <img src={task.img} alt={`${task.task} image`} />
+                  <div className="plan-desc">
+                    <h1>{task.task}</h1>
+                    <p>{task.time} hr.+</p>
+                    <div className="rating-view">
+                      <p>rs.{task.cost}00</p>
+                      <button>View more</button>
+                    </div>
+                  </div>
+                </div>
+                  </>
+                ))
+                }             
+              </div>
+              
+              </> 
+                ))
+              }
+              <div className="map">
+              <Map />
+            </div>
+            
+            
+       
+          
+          
+          
+          </> : <></>
+        }
+        {
+          showdetail === 'task' ? <Task/> : <></>
+        }
+         <PackageLayout/>
+         <TripLayout/>
+      </div>
+    )
+  }
 
 
 //http://localhost:5173/package?days=5&budget=40000&accommodation=Cultural&food=Non-Vegetarian&place=Boudhanath%20Stupa&place=Swayambhunath%20(Monkey%20Temple)&type=Natural&type=Religious
